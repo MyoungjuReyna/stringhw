dc3ecebc9fb7b28b6ab9f538a19d2caab8c9635f
Myoungju Cha <101177202+MyoungjuReyna@users.noreply.github.com>
2023-05-16 21:32:26 -0700

    Update hFour.js

--- hFour.js
+++ hFour.js
@@ -1,18 +1,47 @@
-  var name = prompt('What is your name?');
-  var promptAnswer = "Yesterday  " + name + "  entered the spaceship";
+var canvas = document.getElementById("creativeCoding");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
 
+var x = 0;
+var y = 0;
+var incrementor = 2;
 
-  // Now we have our function with two parameters
-
-function paramters(input1, input2) {
-
-
-// below we will create a piece of code for the answers to show up on the html page.
-
-    var result = input1 + input2;
-    document.getElementById("blankTwo").innerHTML = result;
+function Square() {
+  ctx.fillStyle = "blue";
+  ctx.fillRect(x, y, 100, 100);
 }
 
-// here we have a method that will run the function and we are entering the arguments to pass through.
+function drawBoxxy() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-paramters( promptAnswer , '  and yelled out  ', 'Ganoosh!!');
+// Adding Strings 
+
+  ctx.font = "50px Arial";
+  ctx.fillStyle = "red";
+  ctx.fillText("Wow.. this is amazing!!", 100, 250);
+  ctx.fillText("Bannanas!!", 200, y);
+
+
+//  Passing a variable with text
+  let seriousQuestion = "Fuzzy Wuzzy wasn't fuzzy, was he?";
+  ctx.fillText(seriousQuestion, x, 400);
+
+
+
+
+
+
+
+  Square();
+  while (x + incrementor > canvas.width || x + incrementor < 0) {
+    incrementor = -incrementor;
+  }
+  while (y + incrementor > canvas.height || y + incrementor < 0) {
+    incrementor = -incrementor;
+  }
+  x += incrementor;
+  y += incrementor;
+}
+
+setInterval(drawBoxxy, 10);
